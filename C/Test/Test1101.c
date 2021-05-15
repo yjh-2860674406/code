1bc17d1bd5d77d643aa1255ff479f5db5ae7b66b
yjh <y2860674406@gmail.com>
2021-05-15 19:50:20 +0800

    .

--- C/Test/Test1101.c
+++ C/Test/Test1101.c
@@ -1,7 +1,84 @@
 #include <stdio.h>
 
+int stack_prn[100];
+int X;
+
+void Empty ()
+{
+    X = 0;
+}
+
+void push (int num)
+{
+    stack_prn[X] = num;
+    X++;
+}
+
+int pop ()
+{
+    int temp = stack_prn[X-1];
+    X--;
+    return temp;
+}
+
+int Prn ()
+{
+    char temp;
+    scanf("%c",&temp);
+    // 获取字符串
+    int num = (int)temp-48;
+    // 将字符串转换成数字
+    while (temp != (int)'=')
+    // 遇到‘=’结束循环
+    {
+        getchar();
+        // 吃掉一个字符
+        switch (temp)
+        // switch语句判断是否为运算符
+        {
+            case ('+') :
+            {
+                int x = pop();
+                int y = pop();
+                push(y+x);
+            }
+            break;
+            case ('-') :
+            {
+                int x = pop();
+                int y = pop();
+                push(y-x);
+            }
+            break;
+            case ('*') :
+            {
+                int x = pop();
+                int y = pop();
+                push(y*x);
+            }
+            break;
+            case ('/') :
+            {
+                int x = pop();
+                int y = pop();
+                push(y/x);
+            }
+            break;
+            default :
+            // 不是运算符则将数压入栈
+            push(num);
+        }
+        // 重复
+        scanf("%c", &temp);
+        num = (int)temp-48;
+    }
+    // 返回栈顶
+    return pop();
+}
+
 int main()
 {
-    
+    int x = Prn();
+    printf("%d\n", x);
     return 0;
 }
--- Interview/tree.c
+++ Interview/tree.c
@@ -49,57 +49,126 @@ PtrToNode MakeEmpty (PtrToNode T)
 
 PtrToNode Find (ElementType X, PtrToNode T)
 {
-    if (T == NULL) return NULL;
-    if (X < T->Element) return Find (X, T->Left);
-    else if (X > T->Element) return Find (X, T->Right);
-    else return T;
+    // if (T == NULL) return NULL;
+    // if (X < T->Element) return Find (X, T->Left);
+    // else if (X > T->Element) return Find (X, T->Right);
+    // else return T;
+    while (T != NULL)
+    {
+        if (X < T->Element) T = T->Left;
+        else if (X > T->Element) T = T->Right;
+        else return T;
+    }
+    return T;
 }
 
 PtrToNode FindMin (PtrToNode T)
 {
-    if (T == NULL) return NULL;
-    if (T->Left == NULL) return T;
-    else return FindMin (T->Left);
+    // if (T == NULL) return NULL;
+    // if (T->Left == NULL) return T;
+    // else return FindMin (T->Left);
+    while (T->Left != 0) T = T->Left;
+    return T;
 }
 
 PtrToNode FindMax (PtrToNode T)
 {
-    if (T == NULL) return NULL;
-    if (T->Right == NULL) return T;
-    else return FindMax (T->Right);
+    // if (T == NULL) return NULL;
+    // if (T->Right == NULL) return T;
+    // else return FindMax (T->Right);
+    while (T->Right != 0) T = T->Right;
+    return T;
 }
 
 PtrToNode InSert (ElementType X, PtrToNode T)
 {
-    if (T == NULL)
+    // if (T == NULL)
+    // {
+    //     T = (PtrToNode)malloc(sizeof(Tree));
+    //     T->Left = NULL; T->Right = NULL;
+    //     T->Element = X;
+    // }
+    // if (X < T->Element) T->Left = InSert (X, T->Left);
+    // else if (X > T->Element) T->Right = InSert(X, T->Right);
+    // return T;
+    PtrToNode temp;
+    while (T != NULL)
     {
-        T = (PtrToNode)malloc(sizeof(Tree));
-        T->Left = NULL; T->Right = NULL;
-        T->Element = X;
+        temp = T;
+        if (X < T->Element)
+        {
+            T = T->Left;
+        }
+        else if (X > T->Element)
+        {
+            T = T->Right;
+        }
     }
-    if (X < T->Element) T->Left = InSert (X, T->Left);
-    else if (X > T->Element) T->Right = InSert(X, T->Right);
-    return T;
+    T = (PtrToNode)malloc(sizeof(Tree));
+    T->Element = X; T->Left = NULL; T->Right = NULL;
+    if (X < temp->Element) temp->Left = T;
+    else if (X > temp->Element) temp->Right = T;
 }
 
 PtrToNode Delete (ElementType X, PtrToNode T)
 {
-    PtrToNode Temp;
+    // PtrToNode Temp;
     
-    if (X < T->Element) T->Left = Delete (X, T->Left);
-    else if (X > T->Element) T->Right = Delete (X, T->Right);
-    else if (T->Left && T->Right)
+    // if (X < T->Element) T->Left = Delete (X, T->Left);
+    // else if (X > T->Element) T->Right = Delete (X, T->Right);
+    // else if (T->Left && T->Right)
+    // {
+    //     Temp = FindMin(T->Right);
+    //     T->Element = Temp->Element;
+    //     T->Right = Delete(T->Element, T->Right);
+    // }
+    // else
+    // {
+    //     Temp = T;
+    //     if (T->Left == NULL) T = T->Right;
+    //     else if (T->Right == NULL) T = T->Left;
+    //     free(Temp);
+    // }
+    // return T;
+    PtrToNode temp;
+    while (T != NULL)
     {
-        Temp = FindMin(T->Right);
-        T->Element = Temp->Element;
-        T->Right = Delete(T->Element, T->Right);
+        if (X < T->Element) 
+        {
+            temp = T;
+            T = T->Left;
+        }
+        else if (X > T->Element) 
+        {
+            temp = T;
+            T = T->Right;
+        }
+        else break;
     }
-    else
+    if (T == NULL) return NULL;
+    else if (T->Left == NULL && T->Right == NULL)
     {
-        Temp = T;
-        if (T->Left == NULL) T = T->Right;
-        else if (T->Right == NULL) T = T->Left;
-        free(Temp);
+        if (X < temp->Element) temp->Left = NULL;
+        else temp->Right = NULL;
+        free (T);
     }
-    return T;
+    else if (T->Left != NULL && T->Right != NULL)
+    {
+        T->Element = FindMin(T)->Element;
+        free(FindMin(T));
+    }
+    else 
+    {
+        if (T->Left != NULL) 
+        {
+            if (X < temp->Element) temp->Left = T->Left;
+            else temp->Right = T->Left;
+        }
+        else 
+        {
+            if (X < temp->Element) temp->Left = T->Right;
+            else temp->Right = T->Right;
+        }
+        free (T);
+    }
 }
