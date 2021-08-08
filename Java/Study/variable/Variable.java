fc9a528f2dff3297f3131f8c7b5766038a8ad356
yjh <y2860674406@gmail.com>
2021-08-08 09:02:29 +0800

    .

---
+++ Java/Study/variable/Variable.java
@@ -0,0 +1,11 @@
+package variable;
+
+import java.util.Scanner;
+
+public class Variable {
+    public static void main (String[] args)
+    {
+        int age = 19;
+        System.out.println("My age is " + age);
+    }
+}
